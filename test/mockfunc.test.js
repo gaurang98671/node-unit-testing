873bec661ffaef0374e5470f129052839d0bb92e
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-05-28 05:14:03 +0530

    tested mock callback

--- test/mockfunc.test.js
+++ test/mockfunc.test.js
@@ -6,3 +6,18 @@ test("Testing add function",()=>{
     doAdd(1,2)
     expect(add).toHaveBeenCalledWith(1,2)
 })
+
+function testMockCallback(val, callback){
+    newVal = val + 1
+    callback(newVal)
+}
+
+const mockCallback = jest.fn(x=> x+1)
+
+test("Testing mock callback", ()=>{
+    testMockCallback(1, mockCallback)
+
+    expect(mockCallback.mock.calls.length).toBe(1)
+    expect(mockCallback.mock.calls[0][0]).toBe(2)
+    expect(mockCallback.mock.results[0].value).toBe(3)
+})
